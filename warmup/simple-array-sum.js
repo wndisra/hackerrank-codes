87fd7e382d3c740346ecc07ca33908cde5884ccc
Wanda Ichsanul Isra <wandaisra@gmail.com>
2017-06-08 15:13:02 +0700

    Add simple-array-sum.js code

---
+++ warmup/simple-array-sum.js
@@ -0,0 +1,35 @@
+/**
+ * Wanda Ichsanul Isra
+ * https://www.hackerrank.com/challenges/simple-array-sum
+ */
+
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+var input_stdin = "";
+var input_stdin_array = "";
+var input_currentline = 0;
+
+process.stdin.on('data', function(data) {
+  input_stdin += data;
+});
+
+process.stdin.on('end', function() {
+  input_stdin_array = input_stdin.split("\n");
+  main();
+});
+
+function readLine() {
+  return input_stdin_array[input_currentline++];
+}
+
+/////////////// ignore above this line ////////////////////
+
+function main() {
+  var n = parseInt(readLine());
+  arr = readLine().split(' ');
+  arr = arr.map(Number);
+
+  var sum = arr.reduce((a, b) => a + b, 0);
+  console.log(sum);
+}
